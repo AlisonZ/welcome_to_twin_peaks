acc59c3eac3de3127c93c6443d8411795e905785
AlisonZ <alisonkzerbe@gmail.com>
2017-07-12 16:45:54 -0700

    there is now a text of Locations on the right in header that will take to a new page of a what will be locations

--- src/Router.js
+++ src/Router.js
@@ -1,12 +1,18 @@
 import React from 'react';
-import { Scene, Router } from 'react-native-router-flux';
+import { Scene, Router, Actions } from 'react-native-router-flux';
 import MyMap from './components/MyMap';
 import LocationList from './components/LocationList';
 
 const RouterComponent = () => {
     return (
         <Router sceneStyle={{ paddingTop: 65}}>
-            <Scene key='map' component={MyMap} title="Welcome to Twin Peaks" initial/>
+            <Scene
+            onRight={() => Actions.locationList()}
+            rightTitle='Locations'
+            key='map'
+            component={MyMap}
+            title="Welcome to Twin Peaks"
+            initial />
             <Scene key="locationList" component={LocationList} title="Locations" />
         </Router>
     );
