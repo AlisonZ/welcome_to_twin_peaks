f8ba41cec2106d364125b07415024cca21992785
AlisonZ <alisonkzerbe@gmail.com>
2017-07-14 13:38:00 -0700

    Locations clickable text in map view that takes the user to a list of locations

--- src/Router.js
+++ src/Router.js
@@ -4,13 +4,19 @@ import { Scene, Router, Actions } from 'react-native-router-flux';
 import HelloWorld from './components/helloWorld';
 import MyMap from './components/MyMap';
 import LocationList from './components/LocationList';
-
+import LocationSite from './components/LocationSite';
 const RouterComponent = () => {
     return (
         <Router>
+            <Scene
+                onRight={() => Actions.locationsList()}
+                rightTitle='Locations'
+                key="map"
+                component={MyMap}
+                title="Mappy"
+            />
             <Scene key="helloWorld" component={HelloWorld} title="Hi There" />
-            <Scene key="locations" component={LocationList} title="Locations" />
-            <Scene key="map" component={MyMap} title="Mappy" initial />
+            <Scene key="locationsList" component={LocationList} title="Locations" />
         </Router>
     );
 };
