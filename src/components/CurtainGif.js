53baae8d459b7003ac22c9354f0d9dfe508b5681
AlisonZ <alisonkzerbe@gmail.com>
2017-07-20 21:12:03 -0700

    curtain fade is working, but only sometimes redirects to map. other times just hangs on the curtain component. think the issue is in the timer and not the fade

--- src/components/CurtainGif.js
+++ src/components/CurtainGif.js
@@ -1,21 +1,24 @@
 import React, { Component } from 'react';
 import { View, Dimensions, Image } from 'react-native';
 import { Actions } from 'react-native-router-flux';
+import FadeImage from 'react-native-fade-image'
 var { height, width } = Dimensions.get('window');
+
 
 class CurtainGif extends Component {
     componentDidMount() {
         this.timeoutHandle = setTimeout(() => {
             console.log('i want this to work!');
             Actions.map();
-        }, 1500);
+        }, 2100);
     }
 
     render() {
         return (
             <View style={styles.wrapper}>
-                <Image
+                <FadeImage
                 style={styles.gif}
+                duration = {2100}
                 source={require('../img/curtains.gif')}
                 />
             </View>
